f25d60ed11cf5e77a8abacaab685defc0a12a8e4
Aliveng <aliveng@yandex.ru>
2021-03-01 14:56:34 +0300

    root - LoggedOutViewController. tabBarController после Login или Registr

--- LookLike/AppDelegate.swift
+++ LookLike/AppDelegate.swift
@@ -17,33 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let window = UIWindow()
         self.window = window
         
-        let discoverViewController = DiscoverViewController()
-        discoverViewController.tabBarItem = UITabBarItem.init(title: "", image: .eyeImg, tag: 0)
-        
-        let searchViewController = SearchViewController()
-        searchViewController.tabBarItem = UITabBarItem.init(title: "", image: .searchImg, tag: 1)
-        
-        let profileViewController = ProfileViewController()
-        profileViewController.tabBarItem = UITabBarItem.init(title: "", image: .personImg, tag: 2)
-        
-        let tabBarController = UITabBarController()
-        
-        tabBarController.tabBar.isTranslucent = false
-        tabBarController.tabBar.barTintColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-        tabBarController.tabBar.tintColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-        tabBarController.tabBar.unselectedItemTintColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-        
-        tabBarController.setViewControllers([UINavigationController.init(rootViewController:
-                                                                    discoverViewController),
-                                             UINavigationController.init(rootViewController:
-                                                                    searchViewController),
-                                             UINavigationController.init(rootViewController:
-                                                                    profileViewController)],
-                                                                    animated: true)
-        
-        //        window.rootViewController = UINavigationController(rootViewController: LoggedOutViewController())
-        
-        window.rootViewController = tabBarController
+        window.rootViewController = UINavigationController(rootViewController: LoggedOutViewController())
         window.makeKeyAndVisible()
         
         return true
--- LookLike/Screens/RegisterViewController.swift
+++ LookLike/Screens/RegisterViewController.swift
@@ -133,8 +133,32 @@ class RegisterViewController: UIViewController {
     
     @objc
     private func didTapSignUpButton() {
-        let controller = SearchViewController()
-        navigationController?.pushViewController(controller, animated: true)
+        
+        let discoverViewController = DiscoverViewController()
+        discoverViewController.tabBarItem = UITabBarItem.init(title: "", image: .eyeImg, tag: 0)
+        
+        let searchViewController = SearchViewController()
+        searchViewController.tabBarItem = UITabBarItem.init(title: "", image: .searchImg, tag: 1)
+        
+        let profileViewController = ProfileViewController()
+        profileViewController.tabBarItem = UITabBarItem.init(title: "", image: .personImg, tag: 2)
+        
+        let tabBarController = UITabBarController()
+        
+        tabBarController.tabBar.isTranslucent = false
+        tabBarController.tabBar.barTintColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        tabBarController.tabBar.tintColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        tabBarController.tabBar.unselectedItemTintColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        
+        tabBarController.setViewControllers([UINavigationController.init(rootViewController:
+                                                                    discoverViewController),
+                                             UINavigationController.init(rootViewController:
+                                                                    searchViewController),
+                                             UINavigationController.init(rootViewController:
+                                                                    profileViewController)],
+                                                                    animated: true)
+        tabBarController.modalPresentationStyle = .fullScreen
+        present(tabBarController, animated: true)
         print("Кнопка - переход после регистрации")
     }
 }
